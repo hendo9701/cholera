fd2502ede35d7f04cb89d08412d93b5e0b51c973
hendo9701 <hayder9701@gmail.com>
2020-11-08 00:44:26 -0500

    First commit

---
+++ src/main/kotlin/cholera/views/MainView.kt
@@ -0,0 +1,12 @@
+package cholera.views
+
+import cholera.styles.Styles
+import tornadofx.*
+
+class MainView : View("Hello TornadoFX") {
+    override val root = hbox {
+        label(title) {
+            addClass(Styles.heading)
+        }
+    }
+}
